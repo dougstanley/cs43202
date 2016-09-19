97ad4c7769d451c1d83d4e6aecc6181da9c79805
Doug Stanley <doug@douglasstanley.com>
2016-09-19 19:16:15 -0400

    Added script to test project project 1

---
+++ projects/proj1.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import base64
+
+try:
+    import json
+except:
+    print "Fail"
+    sys.exit(1)
+
+
+def encode(DATA):
+    print "Paste the following into the submission box for this assignment",
+    print "on blackboard"
+    print 79*'='
+    blob = base64.b64encode(json.dumps(DATA))
+    print blob
+    print "\n"
+
+try:
+    DATA = {'user': os.environ.get('USER', '')}
+    DATA['path'] = os.environ.get('PATH', 'NONE')
+    DATA['uname'] = os.uname()
+    DATA['version'] = sys.version
+    DATA['exec'] = sys.executable
+    DATA['modpath'] = sys.path
+    encode(DATA)
+except:
+    print "FAIL!!"
+    sys.exit(1)
